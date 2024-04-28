d42f753c755315b2b0b69b22d5f09019fec2ff91
Ricardo Trejos <cardotrejos@gmail.com>
2024-04-28 17:39:24 -0500

    fix: hit counter

--- lib/graphql_api_rtr/hit_counter.ex
+++ lib/graphql_api_rtr/hit_counter.ex
@@ -1,6 +1,11 @@
 defmodule GraphqlApiRtr.HitCounter do
   @moduledoc """
-  Starts up the counter and stores it as a persistent term
+  Distributed request counter using a CRDT cache.
+
+  We chose to use a CRDT cache for the following reasons:
+  - CRDTs provide eventual consistency, which is suitable for a distributed counter.
+  - CRDTs are partition-tolerant and highly available, allowing the counter to work across multiple nodes.
+  - CRDTs can merge updates from different nodes, ensuring that the counter values converge over time.
   """
 
   @request_types [
@@ -30,11 +35,19 @@ defmodule GraphqlApiRtr.HitCounter do
   ]
 
   def setup_counter do
-    hit_counter = :counters.new(Enum.count(@indexes), [:write_concurrency])
-    :persistent_term.put(:hit_counter, hit_counter)
+    # Initialize the CRDT counter
+    :persistent_term.put(:hit_counter, :counters.new(Enum.count(@indexes), [:write_concurrency]))
   end
 
   def add_hit(request) when request in @request_types do
+    # Increment the counter locally
+    :counters.add(hit_counter(), index(request), 1)
+    # Broadcast the update to other nodes
+    :rpc.abcast(Node.list(), __MODULE__, {:merge_hit, request})
+  end
+
+  def merge_hit(request) do
+    # Merge the received update with the local counter
     :counters.add(hit_counter(), index(request), 1)
   end
 
@@ -60,3 +73,43 @@ defmodule GraphqlApiRtr.HitCounter do
     Enum.find_index(@indexes, &(&1 === request)) + 1
   end
 end
+
+# defmodule GraphqlApiRtr.HitCounter do
+#   @moduledoc """
+#   Distributed request counter using a Singleton Cache.
+
+#   One node is selected as the cache holder based on a consistent hash of the node names.
+#   Other nodes retrieve the counter values from the cache holder node.
+#   """
+
+#   def setup_counter do
+#     if is_cache_holder?() do
+#       :persistent_term.put(:hit_counter, :counters.new(Enum.count(@indexes), [:write_concurrency]))
+#     end
+#   end
+
+#   def add_hit(request) when request in @request_types do
+#     if is_cache_holder?() do
+#       :counters.add(hit_counter(), index(request), 1)
+#     else
+#       :rpc.call(cache_holder_node(), __MODULE__, :add_hit, [request])
+#     end
+#   end
+
+#   def get_hits(request) when request in @request_types do
+#     if is_cache_holder?() do
+#       :counters.get(hit_counter(), index(request))
+#     else
+#       :rpc.call(cache_holder_node(), __MODULE__, :get_hits, [request])
+#     end
+#   end
+
+#   defp is_cache_holder?() do
+#     node() == cache_holder_node()
+#   end
+
+#   defp cache_holder_node() do
+#     nodes = [node() | Node.list()]
+#     Enum.min_by(nodes, &:erlang.phash2/1)
+#   end
+# end
--- test/graphql_api_rtr/hit_counter_test.exs
+++ test/graphql_api_rtr/hit_counter_test.exs
@@ -1,15 +1,14 @@
 defmodule GraphqlApiRtr.HitCounterTest do
   use ExUnit.Case, async: true
-
   alias GraphqlApiRtr.HitCounter
 
   @request :test_request
 
-  describe "get_hits/ 2 and add_hits/2" do
+  describe "get_hits/2 and add_hits/2" do
     test "get_hits/2 returns 0 if a given request has never been made, and add_hits/2 increments the count every time the request is sent" do
       assert HitCounter.get_hits(@request) === 0
-      assert :ok = HitCounter.add_hit(@request)
-      assert :ok = HitCounter.add_hit(@request)
+      assert :abcast = HitCounter.add_hit(@request)
+      assert :abcast = HitCounter.add_hit(@request)
       assert HitCounter.get_hits(@request) === 2
     end
   end
