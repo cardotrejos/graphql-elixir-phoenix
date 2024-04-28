2ede9663ae11f83686ea1cd0c4a72338a4038494
Ricardo Trejos <cardotrejos@gmail.com>
2024-04-28 17:20:18 -0500

    fix: test suite

--- test/graphql_api_rtr_web/auth_plug_test.exs
+++ test/graphql_api_rtr_web/auth_plug_test.exs
@@ -2,15 +2,12 @@ defmodule GraphqlApiRtrWeb.AuthPlugTest do
   use GraphqlApiRtrWeb.ConnCase
   alias GraphqlApiRtrWeb.AuthPlug
 
-  test "puts secret key provided through the HTTP authentication header as context in the Absinthe.Plug under the :absinthe key",
-       %{conn: conn} do
-    conn = Plug.Conn.put_req_header(conn, "authentication", "Secret")
-
-    assert %Plug.Conn{private: %{:absinthe => %{context: %{secret_key: "Secret"}}}} =
-             AuthPlug.call(conn, %{})
+  test "puts secret key provided through the HTTP authorization header as context in the Absinthe.Plug under the :absinthe key", %{conn: conn} do
+    conn = Plug.Conn.put_req_header(conn, "authorization", "Bearer Secret")
+    assert %Plug.Conn{private: %{absinthe: %{context: %{secret_key: "Secret"}}}} = AuthPlug.call(conn, %{})
   end
 
-  test "returns unchanged conn when no authentication header is provided", %{conn: conn} do
+  test "returns unchanged conn when no authorization header is provided", %{conn: conn} do
     assert conn === AuthPlug.call(conn, %{})
   end
 end
